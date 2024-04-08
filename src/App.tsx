185727b588da76f8e4fa6d582e5552fd143d1a09
Animator55 <92542799+Animator55@users.noreply.github.com>
2024-04-08 18:51:20 -0300

    added fade in and blocklights

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import "./assets/App.css"
 
 type projectType = {
@@ -43,80 +44,112 @@ export default function App() {
     },
   ]
 
-  const UserTop = ()=>{
+  const UserTop = () => {
     return <section className='block'>
-      <div className='user-image'>
-        <img />
-      </div>
-      <div>
-        <h1>Nahuel Ibarra</h1>
-        <p>Front End Developer</p>
-      </div>
+      <section className="block-content">
+        <div className='user-image'>
+          <img />
+        </div>
+        <div>
+          <h1>Nahuel Ibarra</h1>
+          <p>Front End Developer</p>
+        </div>
+      </section>
     </section>
   }
-  const TechBlock = ()=>{
+  const TechBlock = () => {
     return <section className="block">
-      <img src='' alt='react'/>
-      <img src='' alt='typescript'/>
-      <img src='' alt='html'/>
-      <img src='' alt='css'/>
-      <img src='' alt='php'/>
+      <section className="block-content">
+        <img src='' alt='react' />
+        <img src='' alt='typescript' />
+        <img src='' alt='html' />
+        <img src='' alt='css' />
+        <img src='' alt='php' />
+      </section>
     </section>
   }
-  const ProjectsListBlock = ()=>{
+  const ProjectsListBlock = () => {
     return <section className="block">
-      {ProjectsList.map(obj=>{
-        return <section className='project-section' key={Math.random()}>
-          <div className='project-img'>
-            <img src={obj.img}/>
-          </div>
-          <div className="project-top">
-            <h2 className='project-title'>{obj.name}</h2>
-            <div className='tags'>
-              {obj.tags.map(tag=>{
-                return <div key={Math.random()}>{tag}</div>
-              })}
+      <section className="block-content">
+        {ProjectsList.map(obj => {
+          return <section className='project-section' key={Math.random()}>
+            <div className='project-img'>
+              <img src={obj.img} />
             </div>
-          </div>
-          <p className='project-desc'>{obj.desc}</p>
-        </section>
-      })}
+            <div className="project-top">
+              <h2 className='project-title'>{obj.name}</h2>
+              <div className='tags'>
+                {obj.tags.map(tag => {
+                  return <div key={Math.random()}>{tag}</div>
+                })}
+              </div>
+            </div>
+            <p className='project-desc'>{obj.desc}</p>
+          </section>
+        })}
+      </section>
     </section>
   }
-  const CareerBlock = ()=>{
+  const CareerBlock = () => {
     return <section className="block">
-      {careerList.map(obj=>{
-        return <section className='career-section' key={Math.random()}>
-          <h2 className='career-title'>{obj.title}</h2>
-          <div className='career-period'>{obj.period}</div>
-          <p className='career-desc'>{obj.desc}</p>
-        </section>
-      })}
+      <section className="block-content">
+        {careerList.map(obj => {
+          return <section className='career-section' key={Math.random()}>
+            <h2 className='career-title'>{obj.title}</h2>
+            <div className='career-period'>{obj.period}</div>
+            <p className='career-desc'>{obj.desc}</p>
+          </section>
+        })}
+      </section>
     </section>
   }
 
-  const AboutMeBlock = ()=>{
+  const AboutMeBlock = () => {
     return <section className='block'>
-      <h1>About Me</h1>
-      <p>fidnaesgkjdnskgm dokjsgn dfsngkjidsngkj hdskljbhnxdfkjs gndmks gklxdsnvgklb dnsmjvgb xdzjbnkjds gmjdsklvg dskj gmkldfnbkjdrnfjg ndkjsfj kdasjvgndkshfjdb dkjsnb vjds gjdmskgbjds gjdnsgkbdesgkji bdnfsjgh dfkskbhldfjkig ndrsoghdmsf hdfsjmg kldsmgdsmgk.</p>
+      <section className="block-content">
+        <h1>About Me</h1>
+        <p>fidnaesgkjdnskgm dokjsgn dfsngkjidsngkj hdskljbhnxdfkjs gndmks gklxdsnvgklb dnsmjvgb xdzjbnkjds gmjdsklvg dskj gmkldfnbkjdrnfjg ndkjsfj kdasjvgndkshfjdb dkjsnb vjds gjdmskgbjds gjdnsgkbdesgkji bdnfsjgh dfkskbhldfjkig ndrsoghdmsf hdfsjmg kldsmgdsmgk.</p>
+      </section>
     </section>
   }
 
-  const ContactBlock = ()=>{
+  const ContactBlock = () => {
     return <section className='block'>
-      <button className='contact-button'>GitHub</button>
-      <button className='contact-button'>LinkedIn</button>
+      <section className="block-content">
+        <button className='contact-button'>GitHub</button>
+        <button className='contact-button'>LinkedIn</button>
+      </section>
     </section>
   }
+
+
+  /////
+
+  const ScrollHandler = ()=>{
+    let blocks = document.querySelectorAll(".block")
+
+    for(let i=0; i<blocks.length; i++) {
+      let windowHeight = window.innerHeight
+      let revealTop = blocks[i].getBoundingClientRect().top
+      let offset = 150
+
+      blocks[i].classList.toggle("fade-in", revealTop < windowHeight - offset)
+    }
+  }
+
+  React.useEffect(()=>{
+    let list = document.querySelector(".list")
+    list?.addEventListener("scroll", ScrollHandler)
+  })
 
   return <main>
     <section className="list">
-      <UserTop/>
-      <TechBlock/>
-      <ProjectsListBlock/>
-      <CareerBlock/>
-      <AboutMeBlock/>
-      <ContactBlock/>
+      <UserTop />
+      <TechBlock />
+      <ProjectsListBlock />
+      <CareerBlock />
+      <AboutMeBlock />
+      <ContactBlock />
     </section>
   </main>
 }
